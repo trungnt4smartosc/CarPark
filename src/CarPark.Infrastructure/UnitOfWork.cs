15e7088b81f21ae87fefc49781d612151d5a46c6
trungnt4 <trungnt4@smartosc.com>
2021-03-09 14:15:15 +0700

    Remove unnecessary code

--- src/CarPark.Infrastructure/UnitOfWork.cs
+++ src/CarPark.Infrastructure/UnitOfWork.cs
@@ -51,55 +51,12 @@ namespace CarPark.Infrastructure
         {
             await _dbContext.SaveChangesAsync();
         }
-        public async Task<List<T>> RawSqlQuery<T>(string query, Func<DbDataReader, T> map)
+
+        public void Dispose()
         {
-            using (var command = _dbContext.Database.GetDbConnection().CreateCommand())
-            {
-                command.CommandText = query;
-                command.CommandType = CommandType.Text;
-
-                _dbContext.Database.OpenConnection();
-
-                using (var result = command.ExecuteReader())
-                {
-                    var entities = new List<T>();
-
-                    while (result.Read())
-                    {
-                        entities.Add(map(result));
-                    }
-
-                    return entities;
-                }
-            }
+            throw new NotImplementedException();
         }
 
         #endregion
-        private bool disposedValue = false; // To detect redundant calls
-
-        protected virtual void Dispose(bool disposing)
-        {
-            if (!disposedValue)
-            {
-                if (disposing)
-                {
-                    // TODO: dispose managed state (managed objects).
-                }
-
-                // TODO: free unmanaged resources (unmanaged objects) and override a finalizer below.
-                // TODO: set large fields to null.
-
-                disposedValue = true;
-            }
-        }
-
-        // This code added to correctly implement the disposable pattern.
-        public void Dispose()
-        {
-            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
-            Dispose(true);
-            // TODO: uncomment the following line if the finalizer is overridden above.
-            // GC.SuppressFinalize(this);
-        }
     }
 }
